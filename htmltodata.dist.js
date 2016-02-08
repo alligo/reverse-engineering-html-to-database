39950046a16a9a2e623cf2ac9e3367c744c27c6e
Emerson Rocha Luiz <emerson@alligo.com.br>
2016-02-08 03:52:11 -0200

    Created HTMLToData.id(), HTMLToData.url(); Created base of data_categories

--- htmltodata.dist.js
+++ htmltodata.dist.js
@@ -8,8 +8,9 @@
  */
 var cheerio = require('cheerio');
 var HTMLToData;
+var data_categories = require('./bootstrap').requireOneOf(['./data_categories.json', './data_categories.dist.json']);
 
-
+console.log(data_categories.rows);
 /**
  * Helper. Remove line breaks and trim
  *
@@ -27,12 +28,48 @@ function clearTrash(item) {
 }
 
 /**
+ * On this example, files for URLs /folder/article-123 was saved as 
+ * /folder/article-123.html, so we reverse this logic
+ *
+ * @param   {String}  relativePath
+ * @returns {String}
+ */
+function url(relativePath) {
+
+  if (relativePath.slice(-5) === '.html') {
+    return relativePath.slice(0, -5);
+  }
+
+  return relativePath;
+}
+
+/**
+ * On this example, files for URLs article IDs was at the end of a parsed URL,
+ * like /folder/article-123
+ *
+ * @param   {String}  url
+ * @returns {String}
+ */
+function id(url) {
+  var uid = null, temp;
+  if (url && url.split) {
+    temp = url.split('-').pop();
+    if (parseInt(temp, 10)) {
+      uid = parseInt(temp, 10);
+    }
+  }
+  return uid;
+}
+
+/**
  * Scrap article data from page of type 1
  * 
- * @param   {\cheerio}   $   cheerio loaded with full htmlstring
+ * @param   {\cheerio}   $    cheerio loaded with full htmlstring
+ * @param   {String}     url  
+ * @param   {Integer}    id  
  * @returns {Object}
  */
-function pageType1($) {
+function pageType1($, url, id) {
   var htmlData = {}, title, created_at, author, articleRoot = $('#k2Container');
 
   if ($('#k2Container .itemTitle').length) {
@@ -44,7 +81,23 @@ function pageType1($) {
   if ($('#k2Container .itemFullText').length) {
     //htmlData.text = clearTrash($('#k2Container .itemFullText').html());
   }
-
+  if (htmlData.title && htmlData.author_raw) {
+    if (url) {
+      htmlData.url_raw = url;
+    }
+    if (id) {
+      htmlData.id = id;
+    }
+  }
+//   -- OK -- `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+//   -- OK -- `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+//   -- OK -- `text` text COLLATE utf8_unicode_ci NOT NULL,
+//  `url_raw` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+//  `category_raw` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+//  `tags_raw` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+//  -- OK -- `author_raw` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+//  `created_at` datetime NOT NULL,
+//  `modified_at` datetime NOT NULL,
   console.log(htmlData);
 
   //return $.html();
@@ -65,7 +118,8 @@ module.exports.parse = function (cb, htmlstring, relativepath) {
   if ($('#k2Container.colunistas-page').length) {
     //htmlData = pageType1($('#k2Container'));
     //htmlData = pageType1(htmlstring);
-    htmlData = pageType1($);
+    htmlData = pageType1($, url(relativepath), id(url(relativepath)));
+    //htmlData = pageType1($, relativepath);
     //console.log(relativepath, htmlData);
   } else {
     // ...
